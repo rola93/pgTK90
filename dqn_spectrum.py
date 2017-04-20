ceb58d24fd9ea78c2879346adf6096a774a3e577
dmelli <dmelli@kona.tech>
2017-04-19 21:56:53 -0300

    visualize false in fit

--- dqn_spectrum.py
+++ dqn_spectrum.py
@@ -113,7 +113,7 @@ if args.mode == 'train':
     log_filename = 'dqn_{}_log.json'.format(args.env_name)
     callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
     callbacks += [FileLogger(log_filename, interval=100)]
-    dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
+    dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000, visualize=False)
 
     # After training is done, we save the final weights one more time.
     dqn.save_weights(weights_filename, overwrite=True)
