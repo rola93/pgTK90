8ec2b851db0777552413b24f132cc26263d17b58
rodrigo.laguna <rodrigo.laguna@fing.edu.uy>
2017-04-22 08:14:10 -0300

    Puse vidas a 0, asi jugas una vez y da game over (notar que vidas son las vidas extra). Agregue que no distinga mayusculas de minusculas e indente con cuatro espacios acorde a PEP-8

--- save_states.py
+++ save_states.py
@@ -31,43 +31,43 @@ manic_miner = ManicMiner(frameskip=4)
 print("\n")
 
 for episode in xrange(20):
-	print("Reseting enviroment.")
-	manic_miner.reset(lives=1)
-	manic_miner.render()
-	done = False
-	print("episode: {}".format(episode))
-	while not done:
-		action = None
-		command = raw_input('Action:')
-		if command == 's':
-			action = 'NOOP'
-		elif command == 'a':
-			action = 'LEFT'
-		elif command == 'd':
-			action = 'RIGHT'
-		elif command == 'w':
-			action = 'UP'
-		elif command == 'q':
-			action = 'LEFTUP'
-		elif command == 'e':
-			action = 'RIGHTUP'
-		elif command == 'p':
-			imsave('checkpoints/{}.jpg'.format(saved), obs)
-			manic_miner.save_state( '{}/{}'.format(folder, saved))
-			print("Saved checkpoint #{}".format(saved))
-			saved += 1
-		elif command == 'l':
-			restore_name = raw_input('Number/name of state to restore:')
-			manic_miner.reset(checkpoint= '{}/{}'.format(folder, restore_name))
-			print("Restored checkpoint #{}".format(restore_name))
-			manic_miner.render()
-			continue
-		elif command == 'm':
-			print("\nClosing. Thank you!")
-			manic_miner.close()
-			exit()
-		else:
-			action = 'NOOP'
+    print("Reseting enviroment.")
+    manic_miner.reset(lives=0)
+    manic_miner.render()
+    done = False
+    print("episode: {}".format(episode))
+    while not done:
+        action = None
+        command = raw_input('Action:').lower()
+        if command == 's':
+            action = 'NOOP'
+        elif command == 'a':
+            action = 'LEFT'
+        elif command == 'd':
+            action = 'RIGHT'
+        elif command == 'w':
+            action = 'UP'
+        elif command == 'q':
+            action = 'LEFTUP'
+        elif command == 'e':
+            action = 'RIGHTUP'
+        elif command == 'p':
+            imsave('checkpoints/{}.jpg'.format(saved), obs)
+            manic_miner.save_state('{}/{}'.format(folder, saved))
+            print("Saved checkpoint #{}".format(saved))
+            saved += 1
+        elif command == 'l':
+            restore_name = raw_input('Number/name of state to restore:')
+            manic_miner.reset(checkpoint='{}/{}'.format(folder, restore_name))
+            print("Restored checkpoint #{}".format(restore_name))
+            manic_miner.render()
+            continue
+        elif command == 'm':
+            print("\nClosing. Thank you!")
+            manic_miner.close()
+            exit()
+        else:
+            action = 'NOOP'
 
-		obs, reward, done, info = manic_miner.step(action)
-		manic_miner.render()
+        obs, reward, done, info = manic_miner.step(action)
+        manic_miner.render()
