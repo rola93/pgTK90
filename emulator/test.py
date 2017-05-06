bac6ddd49102d3df176d92539e94161482bab4d5
rodrigo.laguna <rodrigo.laguna@fing.edu.uy>
2017-05-06 10:47:17 -0300

    guardamos todo... fuerza bruta noma'

--- emulator/test.py
+++ emulator/test.py
@@ -14,15 +14,15 @@ for episode in xrange(200):
         j += 1
         action = random.choice(manic_miner.actions() + ['RIGHT', 'LEFT', 'RIGHT', 'LEFT', 'NOOP', 'RIGHT', 'LEFT', 'NOOP'])
         obs, reward, done, info = manic_miner.step(action)
-        if j % 100 == 0 or j % 100 == 1:
-            print 'guardando episodio ', episode, ' j = ', j
-            imsave('test/{}.jpg'.format(str(episode) + '_' + str(j)), obs)
-        if reward != last_reward:
-            print 'Cambio en recompenza: ', episode, ' j = ', j, 'reward: ', reward
-            imsave('test/rw_{}.jpg'.format(str(reward) + '_' + str(episode) + '_' + str(j)), obs)
+        if j % 100 == 0:
+            print 'episodio = ', episode, ' j = ', j
+        #     imsave('test/{}.jpg'.format(str(episode) + '_' + str(j)), obs)
+        # if reward != last_reward:
+        #     print 'Cambio en recompenza: ', episode, ' j = ', j, 'reward: ', reward
+        imsave('test/rw_{}.jpg'.format(action+'-'+str(reward) + '_' + str(episode) + '_' + str(j)), obs)
         # scipy.misc.imsave('replay/{}.jpg'.format(steps), obs)
-        manic_miner.render()
-        last_reward = reward
-print 'guardando episodio 199 j = ', j
-imsave('test/{}.jpg'.format(str(199) + '_' + str(j)), obs)
+        # manic_miner.render()
+        # last_reward = reward
+# print 'guardando episodio 199 j = ', j
+# imsave('test/{}.jpg'.format(str(199) + '_' + str(j)), obs)
 print "Final del experimento"
