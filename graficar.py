d282101c3c30a576cbae85cfc773453134ac3c21
rodrigo.laguna <rodrigo.laguna@fing.edu.uy>
2017-05-14 11:21:22 -0300

    Actualizamos el script para graficar, para hacerlo mas sencillo

--- graficar.py
+++ graficar.py
@@ -4,87 +4,104 @@ import re
 import json
 
 
-TAG_GREEN = 'MEJOR EN REUNION'
-TAG_RED = 'MEJOR ACTUALMENTE'
+TAG_GREEN = 'Frame skip = 1'
+TAG_RED = 'Frame skip = 2'
 TAG_BLUE = ''
+
+# JSON_GREEN es obligatorio
+JSON_GREEN = 'dqn_BreakoutDeterministic-v3_log.json'
+JSON_RED = None#'dqn_BreakoutDeterministic-v3_log_fs=2.json'
+JSON_BLUE =None# 'otro.json'
+
+# Ejes: valores a graficar en cada eje, son las claves del Json
+EJE_X = 'nb_steps'
+EJE_Y = 'episode_reward'
+
+# Titulo & labels
+TITULO = "Version Actual"
+X_LABEL = "time_steps"
+Y_LABEL = "score"
+
+WINDOW_SIZE = 1
+
 
 def promediar_vector(vector, ventana):
     res = []
     for i in xrange(ventana, len(vector) - ventana):
         res.append(np.average(vector[i-ventana:i+ventana]))
     res = np.array(res)
-    print "Maxima recompenza en ventana: ", np.amax(res)
-    print "Se da en el indice: ", np.argmax(res), " donde vale ", res[np.argmax(res)]
-    print "El ultimo es: ", res[-1]
-    return res
+    max_value = np.amax(res)
+    max_pos = np.argmax(res)
+    return res, max_value, max_pos
 
 
-def graficar(score_green, time_steps_green, window_size, score_red, time_steps_red, score_blue=None, time_steps_blue=None):
-    assert not np.array_equal(score_red,  score_blue)
-    line_up, = plt.plot(time_steps_green[window_size:-window_size], promediar_vector(score_green, window_size), color='green', marker='.',
+def graficar(score_green, time_steps_green, window_size, score_red=None, time_steps_red=None, score_blue=None, time_steps_blue=None):
+
+    vector, max_value, max_pos = promediar_vector(score_green, window_size)
+    plt.plot(time_steps_green[window_size:-window_size], vector, color='green', marker='.',
                         alpha=.5, label=TAG_GREEN)
-    line_down, = plt.plot(time_steps_red[window_size:-window_size], promediar_vector(score_red, window_size), color='red', marker='.', alpha=.5,
-                          label=TAG_RED)
-    if score_blue is not None:
-        third_line = plt.plot(time_steps_blue[window_size:-window_size], promediar_vector(score_blue, window_size), color='blue', marker='.', alpha=.5,
-                          label=TAG_BLUE)
-        plt.legend([TAG_GREEN, TAG_RED, TAG_BLUE], loc='best')
-        # plt.legend(['y = x', 'y = 2x', 'y = 3x', 'y = 4x'])
-    else:
-        plt.legend([TAG_GREEN, TAG_RED], loc='best')
+    used_tags = [TAG_GREEN]
 
-    # plt.plot(time_steps[window_size:-window_size], promediar_vector(score, window_size))
-    plt.xlabel("time_steps")
-    plt.ylabel("score")
-    plt.title("Version Actual")
+    log_msj = "GREEN\nmaximo valor: {}\nposicion: {}".format(max_value, max_pos + window_size)
+    assert np.average(score_green[max_pos:max_pos+window_size+window_size]) == max_value
+
+    if score_red is not None:
+        vector, max_value, max_pos = promediar_vector(score_red, window_size)
+        plt.plot(time_steps_red[window_size:-window_size], vector, color='red', marker='.', alpha=.5,
+                              label=TAG_RED)
+        used_tags.append(TAG_RED)
+
+        log_msj += "\nRED\nmaximo valor: {}\nposicion: {}".format(max_value, max_pos + window_size)
+        assert np.average(score_red[max_pos:max_pos + window_size + window_size]) == max_value
+    if score_blue is not None:
+        vector, max_value, max_pos = promediar_vector(score_blue, window_size)
+        plt.plot(time_steps_blue[window_size:-window_size], vector, color='blue', marker='.', alpha=.5,
+                          label=TAG_BLUE)
+        used_tags.append(TAG_BLUE)
+
+        log_msj += "\nBLUE\nmaximo valor: {}\nposicion: {}".format(max_value, max_pos + window_size)
+        assert np.average(score_blue[max_pos:max_pos + window_size + window_size]) == max_value
+
+    print log_msj
+
+    plt.legend(used_tags, loc='best')
+    plt.xlabel(X_LABEL)
+    plt.ylabel(Y_LABEL)
+    plt.title(TITULO)
     plt.show()
 
-# # Red
-# with open("salidaAviona_20.txt", 'r') as f:
-#     texto = f.read()
-
-# time_steps_viejo, score_viejo = zip(*re.findall(ur"TIMESTEP:\s*(\d+).*?L_REWARD:\s*(\d+)", texto, flags=re.I|re.U))
-
+if not JSON_GREEN:
+    print "JSON_GREEN es obligatorio"
+    exit()
 # Green
-# with open("salidaCosotav2.txt", 'r') as f:
-#     texto = f.read()
+data_GREEN = json.load(open(JSON_GREEN, 'r'))
 
-# time_steps, score = zip(*re.findall(ur"TIMESTEP:\s*(\d+).*?L_REWARD:\s*(\d+)", texto, flags=re.I|re.U))
+try:
+    assert EJE_X in data_GREEN.keys()
+    assert EJE_Y in data_GREEN.keys()
+    print data_GREEN.keys()
+except AssertionError:
+    msj = 'EJE_X y EJE_Y debe ser uno de los siguientes:\n\t'
+    msj += data_GREEN.keys().join('\n\t')
+    msj += '\nEJE_X={}\nEJE_Y={}'.format(EJE_X, EJE_Y)
+    print msj
+    exit()
 
-# time_steps_viejo = time_steps_viejo[:len(time_steps)]
-# score_viejo = score_viejo[:len(score)]
+reward_GREEN = np.array(data_GREEN[EJE_Y])
+step_GREEN = np.array(data_GREEN[EJE_X])
 
-# assert len(score_viejo) == len(score)
+# Red
+data_RED = json.load(open(JSON_RED, 'r')) if JSON_RED else None
+reward_RED = np.array(data_RED[EJE_Y]) if JSON_RED else None
+step_RED = np.array(data_RED[EJE_X]) if JSON_RED else None
 
-#graficar(np.array([int(x) for x in score]), np.array([int(x) for x in time_steps]), 100, np.array([int(x) for x in score_viejo]), np.array([int(x) for x in time_steps_viejo]))
+# Blue
+data_BLUE = json.load(open(JSON_BLUE, 'r')) if JSON_BLUE else None
+reward_BLUE = np.array(data_BLUE[EJE_Y]) if JSON_BLUE else None
+step_BLUE = np.array(data_BLUE[EJE_X]) if JSON_BLUE else None
 
-# Green
-data_DDQN = json.load(open('dqn_BreakoutDeterministic-v3_log.json', 'r'))
-# data_DDQN = json.load(open('dqn_Breakout-v0_log_lun_27_borrado.json', 'r'))
+graficar(score_green=reward_GREEN, time_steps_green=step_GREEN, window_size=WINDOW_SIZE,
+         score_red=reward_RED, time_steps_red=step_RED,
+         score_blue=reward_BLUE, time_steps_blue=step_BLUE)
 
-# # Red
-data_keras = json.load(open('dqn_BreakoutDeterministic-v3_log.json', 'r'))
-#data_keras = json.load(open('miercoles29.json', 'r'))
-# data_tf = json.load(open('tf.json', 'r'))
-
-# # blue
-# data_dueling = json.load(open('dqn_Frame skip casero sin ddqn.json', 'r'))
-
-reward_kerasDDQN = np.array(data_DDQN['episode_reward'])
-step_kerasDDQN = np.array(data_DDQN['nb_steps'])
-
-reward_keras = np.array(data_keras['episode_reward'])
-step_keras = np.array(data_keras['nb_steps'])
-print "Maxima recompenza: ", np.amax(data_keras['episode_reward'])
-print "Se da en el step: ", step_keras[np.argmax(data_keras['episode_reward'])]
-print data_keras.keys()
-
-# reward_keras_dueling = np.array(data_dueling['episode_reward'])
-# step_keras_dueling = np.array(data_dueling['nb_steps'])
-
-# (10, 22650), (100, 22570), (1000, 22085)
-print "--------\n10: ", step_keras[22650+10], "\n100: ", step_keras[22570+100], "\n1000: ", step_keras[22085+1000]
-graficar(score_green=reward_kerasDDQN, time_steps_green=step_kerasDDQN, window_size=1000,
-         score_red=reward_keras, time_steps_red=step_keras)#,
-#          score_blue=reward_keras_dueling, time_steps_blue=step_keras_dueling)
 
