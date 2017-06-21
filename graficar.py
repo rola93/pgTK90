38bd7516baced37d882c1e4924e0defce7aa3604
rodrigo.laguna <rodrigo.laguna@fing.edu.uy>
2017-06-20 21:50:47 -0300

    Graficar para multiples lineas con tazas parametrizadas

--- graficar.py
+++ graficar.py
@@ -4,8 +4,8 @@ import re
 import json
 
 
-TAG_GREEN = 'Frame skip = 1'
-TAG_RED = 'Frame skip = 2'
+TAG_GREEN = 'checkpoints'
+TAG_RED = 'reward'
 TAG_BLUE = ''
 
 # JSON_GREEN es obligatorio
@@ -13,9 +13,18 @@ JSON_GREEN = 'dqn_BreakoutDeterministic-v3_log.json'
 JSON_RED = None#'dqn_BreakoutDeterministic-v3_log_fs=2.json'
 JSON_BLUE =None# 'otro.json'
 
+# 'episode_reward', 'global_score', 'nb_steps', 'mean_absolute_error',
+# 'loss', 'avarage_q', 'mean_eps', 'checkpoint', 'nb_episode_steps', 'duration', 'episode', 'mean_q'
+
 # Ejes: valores a graficar en cada eje, son las claves del Json
 EJE_X = 'nb_steps'
 EJE_Y = 'episode_reward'
+
+# Ejes para multiples colres, ponerle el EJE_Y_RED en None para usar el graficar viejo
+EJE_Y_GREEN = 'checkpoint'
+EJE_Y_RED = 'episode_reward'
+EJE_Y_BLUE = None#'nb_steps'
+
 
 # Titulo & labels
 TITULO = "Version Actual"
@@ -23,6 +32,10 @@ X_LABEL = "episodes"
 Y_LABEL = "episode_reward"
 
 WINDOW_SIZE = 1
+
+GREEN_RATE = 1
+RED_RATE = 1
+BLUE_RATE = 1
 
 
 def promediar_vector(vector, ventana):
@@ -74,6 +87,40 @@ def graficar(score_green, time_steps_green, window_size, score_red=None, time_st
     plt.title(TITULO)
     plt.show()
 
+
+def grafica_multiple(eje_x, eje_y_green, eje_y_red, eje_y_blue=None, window_size=1):
+
+    vector, max_value, max_pos = promediar_vector(eje_y_green, window_size)
+    plt.plot(eje_x[window_size:-window_size], vector, color='green', marker='.',
+             alpha=.5, label=TAG_GREEN)
+    used_tags = [TAG_GREEN]
+
+    log_msj = "GREEN\nmaximo valor: {}\nposicion: {}".format(max_value, max_pos + window_size)
+
+    vector, max_value, max_pos = promediar_vector(eje_y_red, window_size)
+    plt.plot(eje_x[window_size:-window_size], vector, color='red', marker='.', alpha=.5,
+             label=TAG_RED)
+    used_tags.append(TAG_RED)
+
+    log_msj += "\nRED\nmaximo valor: {}\nposicion: {}".format(max_value, max_pos + window_size)
+
+    if eje_y_blue is not None:
+        vector, max_value, max_pos = promediar_vector(eje_y_blue, window_size)
+        plt.plot(eje_x[window_size:-window_size], vector, color='blue', marker='.', alpha=.5,
+                 label=TAG_BLUE)
+        used_tags.append(TAG_BLUE)
+
+        log_msj += "\nBLUE\nmaximo valor: {}\nposicion: {}".format(max_value, max_pos + window_size)
+
+    print log_msj
+
+    plt.legend(used_tags, loc='best')
+    plt.xlabel(X_LABEL)
+    plt.ylabel(Y_LABEL)
+    plt.title(TITULO)
+    plt.show()
+
+
 if not JSON_GREEN:
     print "JSON_GREEN es obligatorio"
     exit()
@@ -86,13 +133,24 @@ try:
     print data_GREEN.keys()
 except AssertionError:
     msj = 'EJE_X y EJE_Y debe ser uno de los siguientes:\n\t'
-    msj += data_GREEN.keys().join('\n\t')
+    msj += '\n\t'.keys().join(data_GREEN)
     msj += '\nEJE_X={}\nEJE_Y={}'.format(EJE_X, EJE_Y)
     print msj
     exit()
 
 reward_GREEN = np.array(data_GREEN[EJE_Y])
 step_GREEN = np.array(data_GREEN[EJE_X])
+
+if EJE_Y_GREEN and EJE_Y_RED:
+    assert EJE_Y_GREEN in data_GREEN.keys()
+    assert EJE_Y_RED in data_GREEN.keys()
+    assert not EJE_Y_BLUE or EJE_Y_BLUE in data_GREEN.keys()
+
+    eje_y_green = np.array(data_GREEN[EJE_Y_GREEN]) * GREEN_RATE
+    eje_y_red = np.array(data_GREEN[EJE_Y_RED]) * RED_RATE
+    eje_y_blue = np.array(data_GREEN[EJE_Y_BLUE]) * BLUE_RATE if EJE_Y_BLUE else None
+    grafica_multiple(step_GREEN, eje_y_green, eje_y_red, eje_y_blue, window_size=WINDOW_SIZE)
+    exit()
 
 # Red
 data_RED = json.load(open(JSON_RED, 'r')) if JSON_RED else None
